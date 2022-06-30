c1e38a6dc40c917279e6b75143af22860365f96f
thiagododti <t.dod@live.com>
2022-06-30 18:49:46 -0300

    estudos javascript

---
+++ javascript/test.js
@@ -0,0 +1,15 @@
+// var y = 0;    <- comentar usando //
+
+/* var x = 1; <- comentar bloco com */
+
+//var desconto = 2 // declaração de variavel
+
+// const PRECO = 2 // declaração de constante Obs boa pratica declarar constante em maiusculo.
+
+//function soma(a, b) { return a + b; } // declaraçao de função
+
+// console.log(); retorna o valor no console
+
+// soma(1, 3); chamada da função 
+
+console.log("ola");
